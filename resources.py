41e94e04fe332108c4055cfcaf4c2a3a0a5e8b91
Jacob <jacob@Paranoid-Android.local>
2019-02-26 22:50:44 -0600

    resources

---
+++ resources.py
@@ -0,0 +1,16 @@
+from random import choices
+from random import randint
+from random import uniform
+
+class Resource:
+    '''
+    Here are a list of available resources:
+    
+    Genetic
+    Synthetics
+    Organics
+    Metals
+    Fuels
+    Science
+    '''
+
